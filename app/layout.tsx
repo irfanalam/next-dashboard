23b53c87dcdd831a2404a6efd34f71b96a682222
Irfan Alam <irfanalam49@gmail.com>
2024-02-29 06:58:38 +0500

    till routing updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import { oswald } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${oswald.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Oswald } from 'next/font/google';
+
+export const oswald = Oswald({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
